4d22ecfdc171f71ad482b55e9ef15b6b322d263c
wakio-ngari <ngarijoyce017@gmail.com>
2024-11-28 11:52:59 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// code your solution here
+function saturdayFun(activity="roller-skate"){
+       return `This Saturday, I want to ${activity}!`;
+
+}
+console.log (saturdayFun());
+console.log(saturdayFun("bathe my dog"))
+
+function mondayWork(activity= "go to the office"){
+    return `This Monday, I will ${activity}.`;
+}
+ console.log(mondayWork());
+   console.log(mondayWork("work from home"));
+
+function wrapAdjective(flair="*"){
+     return function (adjective="special"){
+        return `You are ${flair}${adjective}${flair}!`
+     }
+}
+const encouragingPromptFunction =wrapAdjective("!!!")
+wrapAdjective("%")("a dedicated programmer");
+
